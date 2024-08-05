f9995ba6f9e268609aa722f4bf4d7bdee12686da
Dalois-30 <claudetapi@yahoo.com>
2024-08-05 23:52:41 +0100

    update to s5 okay but without orbitdb

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 export default {
   PORT: process.env.PORT || 3000,
-  S5_NODE_URL: 'https://s5.daltek.tech',
-  S5_CLIENT_PWD: 'Sia#2024',
-  S5_CLIENT_AUTH_TOKEN: 'S5AD7KLaXFLygyUGVbyc9q1gbeprXauGbWhUsokGeYVRiUx'
+  S5_NODE_URL: process.env.S5_NODE_URL,
+  S5_CLIENT_PWD: process.env.S5_CLIENT_PWD,
+  S5_CLIENT_AUTH_TOKEN: process.env.S5_CLIENT_AUTH_TOKEN
 };
